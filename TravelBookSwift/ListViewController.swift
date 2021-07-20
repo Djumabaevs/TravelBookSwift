51f088b5b34ce3dd4857851a25affa5ed6b2918d
BakytDzhumabev <djumabaevb@gmail.com>
2021-07-20 22:35:22 +0600

    add new view controller and segue

---
+++ TravelBookSwift/ListViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ListViewController.swift
+//  TravelBookSwift
+//
+//  Created by Bakyt Dzhumabaev on 20/7/21.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+       
+    }
+    
+
+
+
+}
