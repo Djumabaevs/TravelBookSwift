5d1eab63e4ba6ad256b31b23aa7571e7345a3728
BakytDzhumabev <djumabaevb@gmail.com>
2021-07-21 22:02:06 +0600

    stop updating location

--- TravelBookSwift/ViewController.swift
+++ TravelBookSwift/ViewController.swift
@@ -93,6 +93,16 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
                                         
                                         nameText.text = annotationTitle
                                         commentText.text = annotationSubtitle
+                                        
+                                        
+                                        locationManager.stopUpdatingLocation()
+                                        
+                                        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+                                        
+                                        let region = MKCoordinateRegion(center: coordinate, span: span)
+                                        
+                                        mapView.setRegion(region, animated: true)
+                        
                                     }
                                 }
                             }
@@ -134,6 +144,8 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        if selectedTitle == "" {
         let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
         
         let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
@@ -141,6 +153,7 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         let region = MKCoordinateRegion(center: location, span: span)
         
         mapView.setRegion(region, animated: true)
+        }
         
     }
     
