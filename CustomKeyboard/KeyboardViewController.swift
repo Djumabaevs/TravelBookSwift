e718e6ef3fc1522a6c36377940cd8c47f73377db
BakytDzhumabev <djumabaevb@gmail.com>
2021-07-25 09:05:58 +0600

    add island image to normal state

--- CustomKeyboard/KeyboardViewController.swift
+++ CustomKeyboard/KeyboardViewController.swift
@@ -22,8 +22,8 @@ class KeyboardViewController: UIInputViewController {
         
         
         let customButton = UIButton(type: UIButton.ButtonType.system)
-        customButton.frame  =CGRect(x: 150, y: 50, width: 100, height: 100)
-        
+        customButton.frame  = CGRect(x: 150, y: 50, width: 100, height: 100)
+        customButton.setBackgroundImage(UIImage(named: "island.jpg"), for: UIControl.State.normal)
         
         
         
